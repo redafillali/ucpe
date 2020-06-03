c4ece8bcfa83bcd9540040f4a3e9934a1d6c5903
Reda El Fillali <reda.elfillali@equity.ma>
2020-06-03 15:44:56 +0100

    create parent form

--- wp-content/plugins/assurance/pages/inscription.php
+++ wp-content/plugins/assurance/pages/inscription.php
@@ -1,8 +1,23 @@
 <?php get_header(); ?>
-<form method="post">
-    <div class="form-group">
-        <label for="nom"> Nom* :</label>
-        <ipnut class="form-control" type="text" name="nom" required></ipnut>
+<form method="post" class="row col-sm-12">
+    <div class="form-group col-md-6">
+        <label for="nom">Nom :</label>
+        <input class="form-control" type="text" name="nom" required />
+    </div>
+    <div class="form-group col-md-6">
+        <label for="tel">Téléphone :</label>
+        <input class="form-control" type="tel" name="tel" required />
+    </div>
+    <div class="form-group col-md-6">
+        <label for="email">Email :</label>
+        <input class="form-control" type="email" name="email" required />
+    </div>
+    <div class="form-group col-md-6">
+        <label for="password">Mot de passe :</label>
+        <input class="form-control" type="password" name="password" required />
+    </div>
+    <div class="form-group col-md-12">
+        <input class="form-control btn btn-primary" type="submit" name="submit" value="Envoyer" required />
     </div>
 </form>
 <?php get_header(); ?>
