5e8391d0341dff49ab7cd141c70caf9b4f784d53
Reda El Fillali <reda.elfillali@equity.ma>
2020-06-03 16:46:56 +0100

    create client check email and error

--- wp-content/plugins/assurance/pages/inscription.php
+++ wp-content/plugins/assurance/pages/inscription.php
@@ -1,4 +1,15 @@
-<?php get_header(); ?>
+<?php
+/*
+  * Template neme: Inscription parent
+  */
+if($_POST)  {
+    $error = create_client($_POST);
+    echo $error;
+    if($error) {
+        wp_redirect(get_bloginfo('url') . '/?page=inscrption&type=parent&error='.$error);
+    }
+}
+get_header(); ?>
 <form method="post" class="row col-sm-12">
     <div class="form-group col-md-6">
         <label for="nom">Nom :</label>
@@ -11,13 +22,14 @@
     <div class="form-group col-md-6">
         <label for="email">Email :</label>
         <input class="form-control" type="email" name="email" required />
+        <small id="emailError" class="form-text">Désolé, cette adresse mail à déjà été utilisée</small>
     </div>
     <div class="form-group col-md-6">
         <label for="password">Mot de passe :</label>
         <input class="form-control" type="password" name="password" required />
     </div>
     <div class="form-group col-md-12">
-        <input class="form-control btn btn-primary" type="submit" name="submit" value="Envoyer" required />
+        <button class="form-control btn btn-primary" type="submit">Envoyer</button>
     </div>
 </form>
 <?php get_header(); ?>
