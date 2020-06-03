b6cb38bd1cd04df840130a1e9a606db3f9d5dd95
Reda El Fillali <reda.elfillali@gmail.com>
2020-06-03 14:34:12 +0000

    bootsrap theme

---
+++ wp-content/plugins/assurance/pages/inscription.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<form method="post">
+    <div class="form-group">
+        <label for="nom"> Nom* :</label>
+        <ipnut class="form-control" type="text" name="nom" required></ipnut>
+    </div>
+</form>
+<?php get_header(); ?>
