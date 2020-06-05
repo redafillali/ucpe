71810f9c4a6fa48f847a80277f7196f959c51eff
Reda El Fillali <reda.elfillali@equity.ma>
2020-06-05 16:02:08 +0100

    inscription parent page

--- wp-content/plugins/assurance/pages/inscription.php
+++ wp-content/plugins/assurance/pages/inscription.php
@@ -1,36 +1,39 @@
-<<<<<<< HEAD
 <?php
 /*
   * Template neme: Inscription parent
   */
 if($_POST)  {
     $error = create_client($_POST);
-    echo $error;
-    if($error) {
-        wp_redirect(get_bloginfo('url') . '/?page=inscrption&type=parent&error='.$error);
-    }
 }
 get_header(); ?>
+<div class="row col-md-12">
+    <ul id="progressbar">
+        <li class="active">Création du compte</li>
+        <li>Informations sur les enfants & adhésion</li>
+        <li>Confirmation & Paiement</li>
+    </ul>
+</div>
+<h2>1 - Renseignements concernant le Parent :</h2>
 <form method="post" class="row col-sm-12">
     <div class="form-group col-md-6">
-        <label for="nom">Nom :</label>
-        <input id="nom" class="form-control" type="text" name="nom" required />
+        <label for="nom">Nom et prénom du parent :</label>
+        <input id="nom" class="form-control" type="text" name="nom" value="<?php if(isset($_POST['nom'])) echo $_POST['nom'] ?>" required />
     </div>
     <div class="form-group col-md-6">
-        <label for="tel">Téléphone :</label>
-        <input id="tel" class="form-control" type="tel" name="tel" required />
+        <label for="tel">Téléphone du parent :</label>
+        <input id="tel" class="form-control" type="tel" name="tel" value="<?php if(isset( $_POST['tel'])) echo $_POST['tel'] ?>" required />
     </div>
     <div class="form-group col-md-6">
-        <label for="email">Email :</label>
-        <input id="email" class="form-control" type="email" name="email" required />
-        <small id="emailError" class="form-text <?php if($_GET['error'] == 'mail') echo 'show'; ?>">Désolé, cette adresse mail à déjà été utilisée</small>
+        <label for="email">Adresse mail du parent :</label>
+        <input id="email" class="form-control" type="email" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email'] ?>" required />
+        <small id="emailError" class="form-text <?php if(isset($error) && $error == 'mail') echo 'show'; ?>">Désolé, cette adresse mail à déjà été utilisée</small>
     </div>
     <div class="form-group col-md-6">
         <label for="password">Mot de passe :</label>
         <input id="password" class="form-control" type="password" name="password" required />
     </div>
-    <div class="form-group col-md-12">
-        <button class="form-control btn btn-primary" type="submit">Envoyer</button>
+    <div class="form-group col-md-12 justify-content-center">
+        <button class="btn btn-warning" type="submit">Envoyer</button>
     </div>
 </form>
 <?php get_footer(); ?>
--- wp-content/themes/ucpe-child/functions.php
+++ wp-content/themes/ucpe-child/functions.php
@@ -16,7 +16,7 @@ add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
 
 if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
     function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'wp-bootstrap-starter-bootstrap-css','wp-bootstrap-starter-fontawesome-cdn' ) );
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'wp-bootstrap-starter-bootstrap-css','wp-bootstrap-starter-fontawesome-cdn' ), wp_get_theme()->get( 'Version' ));
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
